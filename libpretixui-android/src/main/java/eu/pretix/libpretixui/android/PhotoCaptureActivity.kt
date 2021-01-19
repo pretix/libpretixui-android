69ce406aa14a8f949086aadf00a9302560c9568b
Raphael Michel <mail@raphaelmichel.de>
2021-01-19 13:43:05 +0100

    Native photos: Fix resolution

--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/PhotoCaptureActivity.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/PhotoCaptureActivity.kt
@@ -1,15 +1,19 @@
 package eu.pretix.libpretixui.android
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.SurfaceTexture
 import android.hardware.usb.UsbDevice
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.util.Rational
 import android.util.Size
 import android.view.Menu
 import android.view.Surface
@@ -17,10 +21,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
+import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -29,6 +30,9 @@ import com.serenegiant.usb.UVCCamera
 import eu.pretix.libpretixui.android.uvc.CameraDialog
 import kotlinx.android.synthetic.main.activity_photo_capture.*
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executors
@@ -59,7 +63,7 @@ class PhotoCaptureActivity : CameraDialog.CameraDialogParent, AppCompatActivity(
     private var usbMonitor: USBMonitor? = null
     private var uvcCamera: UVCCamera? = null
     private var uvcPreviewSurface: Surface? = null
-    val executorService = Executors.newFixedThreadPool(1)
+    val executorService = Executors.newFixedThreadPool(3)
 
 
     private val onDeviceConnectListener = object : USBMonitor.OnDeviceConnectListener {
@@ -160,29 +164,51 @@ class PhotoCaptureActivity : CameraDialog.CameraDialogParent, AppCompatActivity(
     }
 
     private fun takePhoto() {
-        val imageCapture = imageCapture ?: return
-
         val photoFile = File(
                 outputDirectory,
                 SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis()) + ".jpg"
         )
+        if (requestedCameraString == "front" || requestedCameraString == "back") {
+            val imageCapture = imageCapture ?: return
 
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
-                .build()
+            imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
+                override fun onCaptureSuccess(image: ImageProxy) {
+                    super.onCaptureSuccess(image)
 
-        imageCapture.takePicture(
-                outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
-            override fun onError(exc: ImageCaptureException) {
-                Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    try {
+                        val buffer: ByteBuffer = image.planes[0].buffer
+                        val bytes = ByteArray(buffer.remaining())
+                        buffer.get(bytes)
+                        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+                        val cropped = Bitmap.createBitmap(bitmap, image.cropRect.left, image.cropRect.top, image.cropRect.width(), image.cropRect.height())
+                        val scaled = Bitmap.createScaledBitmap(cropped, STORAGE_RES_W, STORAGE_RES_H, true)
+
+                        FileOutputStream(photoFile).use { out ->
+                            scaled.compress(Bitmap.CompressFormat.JPEG, 98, out)
+                        }
+                    } finally {
+                        image.close()
+                    }
+                }
+
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+            })
+        } else {
+            // usb
+            executorService.execute {
+                val bm = uvcTexture.captureStillImage()
+                try {
+                    FileOutputStream(photoFile).use { out ->
+                        bm.compress(Bitmap.CompressFormat.JPEG, 98, out) // bmp is your Bitmap instance
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
             }
-
-            override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                val savedUri = Uri.fromFile(photoFile)
-                val msg = "Photo capture succeeded: $savedUri"
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                Log.d(TAG, msg)
-            }
-        })
+        }
 
     }
 
@@ -214,8 +240,8 @@ class PhotoCaptureActivity : CameraDialog.CameraDialogParent, AppCompatActivity(
 
                 imageCapture = ImageCapture.Builder()
                         .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                        .setTargetResolution(Size(STORAGE_RES_W, STORAGE_RES_H))
                         .build()
+                imageCapture!!.setCropAspectRatio(Rational(STORAGE_RES_W, STORAGE_RES_H))
 
                 // Select back camera as a default
                 val cameraSelector = if (requestedCameraString == "front" && hasFrontCamera()) {
