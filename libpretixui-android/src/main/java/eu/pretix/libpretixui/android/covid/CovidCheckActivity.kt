ace4b273d0588aeb655d286f48bd3acf2837774c
Martin Gross <gross@rami.io>
2021-07-06 17:14:06 +0200

    More DGC validation

--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/CovidCheckActivity.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/CovidCheckActivity.kt
@@ -3,9 +3,17 @@ package eu.pretix.libpretixui.android.covid
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.ncorti.slidetoact.SlideToActView
+import de.rki.covpass.sdk.cert.*
+import de.rki.covpass.sdk.cert.models.Recovery
+import de.rki.covpass.sdk.cert.models.RecoveryCertType
+import de.rki.covpass.sdk.cert.models.TestCertType
+import de.rki.covpass.sdk.cert.models.VaccinationCertType
+import de.rki.covpass.sdk.utils.isValid
 import eu.pretix.libpretixui.android.R
 import eu.pretix.libpretixui.android.databinding.ActivityCovidCheckBinding
 import eu.pretix.libpretixui.android.scanning.HardwareScanner
@@ -23,6 +31,9 @@ class CovidCheckActivity : AppCompatActivity() {
         val EXTRA_SETTINGS = "settings"
     }
 
+    lateinit var binding: ActivityCovidCheckBinding
+    var checkData = "type: manual"
+
     private val hardwareScanner = HardwareScanner(object : ScanReceiver {
         override fun scanResult(result: String) {
             handleScan(result)
@@ -32,10 +43,18 @@ class CovidCheckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = DataBindingUtil.setContentView<ActivityCovidCheckBinding>(this, R.layout.activity_covid_check)
+        binding = DataBindingUtil.setContentView<ActivityCovidCheckBinding>(this, R.layout.activity_covid_check)
+
+        if (intent.extras?.containsKey(EXTRA_SETTINGS) == true) {
+            binding.settings = intent.extras?.getSerializable(EXTRA_SETTINGS) as CovidCheckSettings
+        } else {
+            finish()
+        }
 
         binding.name = intent.extras?.getString(EXTRA_NAME)
         binding.hasHardwareScanner = intent.extras?.getBoolean(EXTRA_HARDWARE_SCAN, false) ?: false
+        binding.acceptBarcode = binding.settings!!.accept_baercode || binding.settings!!.accept_eudgc
+
         if (intent.extras?.containsKey(EXTRA_BIRTHDATE) == true) {
             try {
                 binding.birthdate = LocalDate.parse(intent.extras!!.getString(EXTRA_BIRTHDATE))
@@ -43,15 +62,10 @@ class CovidCheckActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
-        if (intent.extras?.containsKey(EXTRA_SETTINGS) == true) {
-            binding.settings = intent.extras?.getSerializable(EXTRA_SETTINGS) as CovidCheckSettings
-        } else {
-            finish()
-        }
 
         staConfirm.onSlideCompleteListener = object : SlideToActView.OnSlideCompleteListener {
             override fun onSlideComplete(view: SlideToActView) {
-                setResult(Activity.RESULT_OK, Intent().putExtra(RESULT_CODE, "type: manual"))
+                setResult(Activity.RESULT_OK, Intent().putExtra(RESULT_CODE, checkData))
                 finish()
             }
         }
@@ -68,6 +82,81 @@ class CovidCheckActivity : AppCompatActivity() {
     }
 
     fun handleScan(result: String) {
-        DGC().check(result)
+        try {
+            val dgcResult = DGC().check(result)
+            val dgcEntry = dgcResult.first
+            val covCertificate = dgcResult.second
+
+            checkData = "type: DGC"
+            tvScannedDataPersonName.text = covCertificate.fullName
+            tvScannedDataPersonDetails.text = covCertificate.birthDate.birthDate.toString()
+            tvScannedDataMinDate.text = covCertificate.validFrom.toString()
+            tvScannedDataMaxDate.text = covCertificate.validUntil.toString()
+            binding.hasScannedResult = true
+            binding.hasAcceptableScannedResult = true
+
+            when (dgcEntry.type) {
+                VaccinationCertType.VACCINATION_FULL_PROTECTION -> {
+                    hideAllBut("vacc")
+                }
+                TestCertType.NEGATIVE_PCR_TEST -> {
+                    hideAllBut("tested")
+                }
+                TestCertType.NEGATIVE_ANTIGEN_TEST -> {
+                    hideAllBut("tested2")
+                }
+                RecoveryCertType.RECOVERY -> {
+                    hideAllBut("cured")
+                    if (!isValid((dgcEntry as Recovery).validFrom, dgcEntry.validUntil)) {
+                        binding.hasAcceptableScannedResult = false
+                    }
+                }
+                else -> {
+                    binding.hasAcceptableScannedResult = false
+                    hideAllBut()
+                }
+            }
+            assertGermanValidationRuleSet(dgcResult.second.dgcEntry)
+        } catch (exception: ValidationRuleViolationException) {
+            binding.hasAcceptableScannedResult = false
+            Toast.makeText(applicationContext, "DGC ValidationRuleViolationException", Toast.LENGTH_SHORT).show()
+        } catch (exception: BadCoseSignatureException) {
+            binding.hasAcceptableScannedResult = false
+            Toast.makeText(applicationContext, "DGC BadCoseSignatureException", Toast.LENGTH_SHORT).show()
+        } catch (exception: ExpiredCwtException) {
+            binding.hasAcceptableScannedResult = false
+            Toast.makeText(applicationContext, "DGC ExpiredCwtException", Toast.LENGTH_SHORT).show()
+        } catch (exception: NoMatchingExtendedKeyUsageException) {
+            binding.hasAcceptableScannedResult = false
+            Toast.makeText(applicationContext, "DGC NoMatchingExtendedKeyUsageException", Toast.LENGTH_SHORT).show()
+        } catch (exception: Exception) {
+            // Here we would normally run the next validator
+            hideAllBut()
+            binding.hasScannedResult = true
+            binding.hasAcceptableScannedResult = false
+            Toast.makeText(applicationContext, "Not a DGC", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun hideAllBut(certType: String = "") {
+        clVacc.visibility = View.GONE
+        clCured.visibility = View.GONE
+        clTested.visibility = View.GONE
+        clTested2.visibility = View.GONE
+
+        when (certType) {
+            "vacc" -> {
+                clVacc.visibility = View.VISIBLE
+            }
+            "cured" -> {
+                clCured.visibility = View.VISIBLE
+            }
+            "tested" -> {
+                clTested.visibility = View.VISIBLE
+            }
+            "tested2" -> {
+                clTested2.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/DGC.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/DGC.kt
@@ -1,81 +1,21 @@
 package eu.pretix.libpretixui.android.covid
 
-import android.util.Log
 import de.rki.covpass.sdk.cert.*
 import de.rki.covpass.sdk.cert.models.*
 import de.rki.covpass.sdk.dependencies.sdkDeps
-import de.rki.covpass.sdk.utils.isValid
 
 
 class DGC() {
-    fun check(qrContent: String) {
+    fun check(qrContent: String): Pair<DGCEntry, CovCertificate> {
 
         val qrCoder: QRCoder = sdkDeps.qrCoder
 
         try {
             val covCertificate = qrCoder.decodeCovCert(qrContent)
             val dgcEntry = covCertificate.dgcEntry
-            assertGermanValidationRuleSet(dgcEntry)
-            when (dgcEntry) {
-                is Vaccination -> {
-                    when (dgcEntry.type) {
-                        VaccinationCertType.VACCINATION_FULL_PROTECTION -> {
-                            Log.d("FOOBAR", "VACCINATION_FULL_PROTECTION - OK")
-//                            eventNotifier {
-//                                onValidationSuccess(covCertificate)
-//                            }
-                        }
-                        else -> {
-                            Log.d("FOOBAR", "VACCINATION_FULL_PROTECTION - FAIL")
-//                            eventNotifier {
-//                                onValidationFailure()
-//                            }
-                        }
-                    }
-                }
-                is Test -> {
-                    when (dgcEntry.type) {
-                        TestCertType.NEGATIVE_PCR_TEST -> {
-                            Log.d("FOOBAR", "NEGATIVE_PCR_TEST")
-//                            handleNegativePcrResult(covCertificate)
-                        }
-                        TestCertType.POSITIVE_PCR_TEST -> {
-                            Log.d("FOOBAR", "POSITIVE_PCR_TEST")
-//                            eventNotifier { onValidationFailure() }
-                        }
-                        TestCertType.NEGATIVE_ANTIGEN_TEST -> {
-                            Log.d("FOOBAR", "NEGATIVE_ANTIGEN_TEST")
-//                            handleNegativeAntigenResult(covCertificate)
-                        }
-                        TestCertType.POSITIVE_ANTIGEN_TEST -> {
-                            Log.d("FOOBAR", "POSITIVE_ANTIGEN_TEST")
-//                            eventNotifier { onValidationFailure() }
-                        }
-                        // .let{} to enforce exhaustiveness
-                    }.let {}
-                }
-                is Recovery -> {
-                    if (isValid(dgcEntry.validFrom, dgcEntry.validUntil)) {
-                        Log.d("FOOBAR", "RECOVERY - OK")
-//                        eventNotifier { onValidationSuccess(covCertificate) }
-                    } else {
-                        Log.d("FOOBAR", "RECOVERY - NOK")
-//                        eventNotifier { onValidationFailure() }
-                    }
-                }
-                // .let{} to enforce exhaustiveness
-            }.let {}
+            return Pair(dgcEntry, covCertificate)
         } catch (exception: Exception) {
-            when (exception) {
-                is BadCoseSignatureException,
-                is ExpiredCwtException,
-                is NoMatchingExtendedKeyUsageException -> {
-                    Log.d("FOOBAR", "VALIDATION FAILURE")
-//                    Lumber.e(exception)
-//                    eventNotifier { onValidationFailure() }
-                }
-                else -> throw exception
-            }
+            throw exception
         }
     }
 }
