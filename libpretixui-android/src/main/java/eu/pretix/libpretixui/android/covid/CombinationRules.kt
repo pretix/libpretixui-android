162aaf6e107840fea242d68d6c79b3d12189f5f4
Raphael Michel <mail@raphaelmichel.de>
2022-01-05 15:13:41 +0100

    We should not use . as a separator in JSON logic

--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/CombinationRules.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/covid/CombinationRules.kt
@@ -48,15 +48,15 @@ val sampleRuleBadenWuerttembergDecember2021 = """
     
         {"and" : [
             { "var" : "VACC" },
-            { "!=" : [ { "var": "VACC.occurence_days_since" }, null ] },
-            { "<=" : [ { "var": "VACC.occurence_days_since" }, 180 ] }
+            { "!=" : [ { "var": "VACC_occurence_days_since" }, null ] },
+            { "<=" : [ { "var": "VACC_occurence_days_since" }, 180 ] }
         ]},
     
         { "var" : "CURED" },
     
         {"and" : [
             { "var" : "VACC" },
-            { "var": "VACC.isBooster" }
+            { "var": "VACC_isBooster" }
         ]},
     
         {"and" : [
@@ -79,20 +79,20 @@ class CombinationRules(val logic: String) {
         if (results.containsKey(Proof.VACC)) {
             data.put("VACC", true)
             val cert = results[Proof.VACC]!!.dgcEntry as? Vaccination
-            data.put("VACC.targetDisease", cert?.targetDisease)
-            data.put("VACC.vaccineCode", cert?.vaccineCode)
-            data.put("VACC.product", cert?.product)
-            data.put("VACC.manufacturer", cert?.manufacturer)
-            data.put("VACC.doseNumber", cert?.doseNumber)
-            data.put("VACC.totalSerialDoses", cert?.totalSerialDoses)
-            data.put("VACC.occurence_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.occurrence, LocalDate.now()))
-            data.put("VACC.country", cert?.country)
-            data.put("VACC.certificateIssuer", cert?.certificateIssuer)
-            data.put("VACC.isComplete", cert?.isComplete)
-            data.put("VACC.isCompleteSingleDose", cert?.isCompleteSingleDose)
-            data.put("VACC.isBooster", cert?.isBooster)
-            data.put("VACC.hasFullProtectionAfterRecovery", cert?.hasFullProtectionAfterRecovery)
-            data.put("VACC.hasFullProtection", cert?.hasFullProtection)
+            data.put("VACC_targetDisease", cert?.targetDisease)
+            data.put("VACC_vaccineCode", cert?.vaccineCode)
+            data.put("VACC_product", cert?.product)
+            data.put("VACC_manufacturer", cert?.manufacturer)
+            data.put("VACC_doseNumber", cert?.doseNumber)
+            data.put("VACC_totalSerialDoses", cert?.totalSerialDoses)
+            data.put("VACC_occurence_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.occurrence, LocalDate.now()))
+            data.put("VACC_country", cert?.country)
+            data.put("VACC_certificateIssuer", cert?.certificateIssuer)
+            data.put("VACC_isComplete", cert?.isComplete)
+            data.put("VACC_isCompleteSingleDose", cert?.isCompleteSingleDose)
+            data.put("VACC_isBooster", cert?.isBooster)
+            data.put("VACC_hasFullProtectionAfterRecovery", cert?.hasFullProtectionAfterRecovery)
+            data.put("VACC_hasFullProtection", cert?.hasFullProtection)
         } else {
             data.put("VACC", false)
         }
@@ -100,12 +100,12 @@ class CombinationRules(val logic: String) {
         if (results.containsKey(Proof.CURED)) {
             data.put("CURED", true)
             val cert = results[Proof.CURED]!!.dgcEntry as? Recovery
-            data.put("CURED.targetDisease", cert?.targetDisease)
-            data.put("CURED.firstResult_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.firstResult, LocalDate.now()))
-            data.put("CURED.validFrom_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.validFrom, LocalDate.now()))
-            data.put("CURED.validUntil_days_until", if (cert == null) null else ChronoUnit.DAYS.between(LocalDate.now(), cert.validUntil))
-            data.put("CURED.country", cert?.country)
-            data.put("CURED.certificateIssuer", cert?.certificateIssuer)
+            data.put("CURED_targetDisease", cert?.targetDisease)
+            data.put("CURED_firstResult_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.firstResult, LocalDate.now()))
+            data.put("CURED_validFrom_days_since", if (cert == null) null else ChronoUnit.DAYS.between(cert.validFrom, LocalDate.now()))
+            data.put("CURED_validUntil_days_until", if (cert == null) null else ChronoUnit.DAYS.between(LocalDate.now(), cert.validUntil))
+            data.put("CURED_country", cert?.country)
+            data.put("CURED_certificateIssuer", cert?.certificateIssuer)
         } else {
             data.put("CURED", false)
         }
@@ -113,16 +113,16 @@ class CombinationRules(val logic: String) {
         if (results.containsKey(Proof.TESTED_PCR)) {
             data.put("TESTED_PCR", true)
             val cert = results[Proof.TESTED_PCR]!!.dgcEntry as? TestCert
-            data.put("TESTED_PCR.targetDisease", cert?.targetDisease)
-            data.put("TESTED_PCR.testType", cert?.testType)
-            data.put("TESTED_PCR.testName", cert?.testName)
-            data.put("TESTED_PCR.manufacturer", cert?.manufacturer)
-            data.put("TESTED_PCR.sampleCollection_hours_since", if (cert == null) null else ChronoUnit.HOURS.between(cert.sampleCollection, ZonedDateTime.now()))
-            data.put("TESTED_PCR.testResult", cert?.testResult)
-            data.put("TESTED_PCR.testingCenter", cert?.testingCenter)
-            data.put("TESTED_PCR.country", cert?.country)
-            data.put("TESTED_PCR.certificateIssuer", cert?.certificateIssuer)
-            data.put("TESTED_PCR.isPositive", cert?.isPositive)
+            data.put("TESTED_PCR_targetDisease", cert?.targetDisease)
+            data.put("TESTED_PCR_testType", cert?.testType)
+            data.put("TESTED_PCR_testName", cert?.testName)
+            data.put("TESTED_PCR_manufacturer", cert?.manufacturer)
+            data.put("TESTED_PCR_sampleCollection_hours_since", if (cert == null) null else ChronoUnit.HOURS.between(cert.sampleCollection, ZonedDateTime.now()))
+            data.put("TESTED_PCR_testResult", cert?.testResult)
+            data.put("TESTED_PCR_testingCenter", cert?.testingCenter)
+            data.put("TESTED_PCR_country", cert?.country)
+            data.put("TESTED_PCR_certificateIssuer", cert?.certificateIssuer)
+            data.put("TESTED_PCR_isPositive", cert?.isPositive)
         } else {
             data.put("TESTED_PCR", false)
         }
@@ -130,16 +130,16 @@ class CombinationRules(val logic: String) {
         if (results.containsKey(Proof.TESTED_AG_UNKNOWN)) {
             data.put("TESTED_AG_UNKNOWN", true)
             val cert = results[Proof.TESTED_AG_UNKNOWN]!!.dgcEntry as? TestCert
-            data.put("TESTED_AG_UNKNOWN.targetDisease", cert?.targetDisease)
-            data.put("TESTED_AG_UNKNOWN.testType", cert?.testType)
-            data.put("TESTED_AG_UNKNOWN.testName", cert?.testName)
-            data.put("TESTED_AG_UNKNOWN.manufacturer", cert?.manufacturer)
-            data.put("TESTED_AG_UNKNOWN.sampleCollection_hours_since", if (cert == null) null else ChronoUnit.HOURS.between(cert.sampleCollection, ZonedDateTime.now()))
-            data.put("TESTED_AG_UNKNOWN.testResult", cert?.testResult)
-            data.put("TESTED_AG_UNKNOWN.testingCenter", cert?.testingCenter)
-            data.put("TESTED_AG_UNKNOWN.country", cert?.country)
-            data.put("TESTED_AG_UNKNOWN.certificateIssuer", cert?.certificateIssuer)
-            data.put("TESTED_AG_UNKNOWN.isPositive", cert?.isPositive)
+            data.put("TESTED_AG_UNKNOWN_targetDisease", cert?.targetDisease)
+            data.put("TESTED_AG_UNKNOWN_testType", cert?.testType)
+            data.put("TESTED_AG_UNKNOWN_testName", cert?.testName)
+            data.put("TESTED_AG_UNKNOWN_manufacturer", cert?.manufacturer)
+            data.put("TESTED_AG_UNKNOWN_sampleCollection_hours_since", if (cert == null) null else ChronoUnit.HOURS.between(cert.sampleCollection, ZonedDateTime.now()))
+            data.put("TESTED_AG_UNKNOWN_testResult", cert?.testResult)
+            data.put("TESTED_AG_UNKNOWN_testingCenter", cert?.testingCenter)
+            data.put("TESTED_AG_UNKNOWN_country", cert?.country)
+            data.put("TESTED_AG_UNKNOWN_certificateIssuer", cert?.certificateIssuer)
+            data.put("TESTED_AG_UNKNOWN_isPositive", cert?.isPositive)
         } else {
             data.put("TESTED_AG_UNKNOWN", false)
         }
