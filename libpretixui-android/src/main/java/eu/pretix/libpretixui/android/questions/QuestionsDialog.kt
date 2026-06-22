ed3108628114d1796a88bb0430d36202ecbc8186
Martin Gross <gross@pretix.eu>
2026-06-22 11:39:28 +0200

    QuestionsDialog: Actually display attendeeDOB

--- libpretixui-android/src/main/java/eu/pretix/libpretixui/android/questions/QuestionsDialog.kt
+++ libpretixui-android/src/main/java/eu/pretix/libpretixui/android/questions/QuestionsDialog.kt
@@ -161,7 +161,7 @@ class QuestionsDialog(
         if (this.attendeeDOB.isNullOrBlank()) {
             v.findViewById<TextView>(R.id.tvAttendeeDOB).visibility = View.GONE
         } else {
-            v.findViewById<TextView>(R.id.tvAttendeeDOB).text = attendeeName
+            v.findViewById<TextView>(R.id.tvAttendeeDOB).text = attendeeDOB
         }
         if (ticketType.isNullOrBlank()) {
             v.findViewById<TextView>(R.id.tvTicketType).visibility = View.GONE
